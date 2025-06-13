6d05ed8cb3f07adc13383e91e703a263363ffed0
Tu <vohoangtu@tuzy.vn>
2025-06-13 17:13:52 +0700

    add

--- templates/news/news_tpl.php
+++ templates/news/news_tpl.php
@@ -1,21 +1,1 @@
-<div class="section-main">
-    <div class="wrapper">
-        <div class="content-main">
-            <?php if (!empty($news)) { ?>
-                <?= $custom->titleContainer($titleMain) ?>
-                <div class="row row-news">
-                    <?php foreach ($news as $k => $v) {
-                        echo $custom->news($v, "col-news col-xl-4 col-lg-3 col-md-6 col-12");
-                    } ?>
-                </div>
-            <?php } else { ?>
-                <div class="alert alert-warning w-100" role="alert">
-                    <strong><?= khongtimthayketqua ?></strong>
-                </div>
-            <?php } ?>
-            <div class="w-100">
-                <div class="pagination-home w-100"><?= (!empty($paging)) ? $paging : '' ?></div>
-            </div>
-        </div>
-    </div>
-</div>
+<div class="section-main">    <div class="wrapper">        <div class="content-main">            <?php if (!empty($news)) { ?>                <?= $custom->titleContainer($titleMain) ?>                <div class="row row-news">                    <?php foreach ($news as $k => $v) {                        echo $custom->news($v, "col-news col-news-detail col-xl-6 col-lg-6 col-md-6 col-12");                    } ?>                </div>            <?php } else { ?>                <div class="alert alert-warning w-100" role="alert">                    <strong><?= khongtimthayketqua ?></strong>                </div>            <?php } ?>            <div class="w-100">                <div class="pagination-home w-100"><?= (!empty($paging)) ? $paging : '' ?></div>            </div>        </div>    </div></div>
